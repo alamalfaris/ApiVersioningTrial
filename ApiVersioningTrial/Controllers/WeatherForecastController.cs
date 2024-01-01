fa061e2caaac33a1661e2de54c851e71c3ee3362
Alam Alfaris <alamalfaris97@gmail.com>
2024-01-01 09:38:32 +0700

    Add project files.

---
+++ ApiVersioningTrial/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningTrial.Controllers
+{
+    [ApiVersion(1)]
+    [ApiVersion(2)]
+    [ApiController]
+    [Route("api/v{v:apiVersion}/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [MapToApiVersion(1)]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetV1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [MapToApiVersion(2)]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetV2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
